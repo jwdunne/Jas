5babdf4d96e0cb124ba6c7249f75b75e4af815d4
James <me@jameswdunne.com>
2011-07-07 21:55:50 +0100

    Replicated CSS. Now to improve it

--- src/jas.js
+++ src/jas.js
@@ -1,3 +1,61 @@
 (function () {
-  console.log(Sizzle('h3'));
+  "use strict";
+  function select(selector) {
+    return Sizzle(selector);
+  }
+  
+  function camelCaseCSS(str) {
+    var segments = str.split('-'),
+        segsLen = segments.length,
+        i,
+        newString = '',
+        currSeg;
+    
+    // Camel case all segments except the first.
+    // Probably could do with some optimisation.
+    newString = segments[0];
+    for (i = 1; i < segsLen; i++) {
+      currSeg = segments[i]
+      newString = newString 
+                + currSeg.substring(0, 1).toUpperCase()
+                + currSeg.substring(1);
+    }
+    return newString;
+  }
+  
+  function jas(rules) {
+    var rule,
+        properties,
+        elements,
+        elementsLen,
+        i,
+        propName, jsPropName;
+        
+        
+    for (rule in rules) {
+      properties  = rules[rule];
+      elements    = select(rule);
+      elementsLen = elements.length;
+      for (i = 0; i < elementsLen; i++) {
+        for (propName in properties) {
+          jsPropName = camelCaseCSS(propName);
+          elements[i].style[jsPropName] = properties[propName];
+        }
+      }
+    }
+  }
+  
+  jas.rgb = function (r, g, b) {
+    return "rgb(" + r + ", " + g + ", " + b + ")";
+  };
+  
+  jas.html = function (element) {
+    console.log(this);
+    return 'one';
+  };
+  
+  jas.ELEMENTS
+  
+  // Global it!
+  window.jas = jas;
 }());
