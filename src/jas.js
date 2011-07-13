e114de67c051e2f6b548ec13ee49a47d6535437c
James Dunne <me@jameswdunne.com>
2011-07-13 21:18:15 +0100

    Did some cleaning. Very minor though. Explained some sketchy stuff too

--- src/jas.js
+++ src/jas.js
@@ -1,29 +1,33 @@
 (function () {
   "use strict";
-  
+   
   function select(selector, context) {
     return Sizzle(selector, context);
   }
-  
+   
+  /**
+   * Camel-case CSS property names
+   * 
+   * We need to do this because JS properties equivilent to CSS are
+   * all in camelCase due to language restrictions.
+   * 
+   * @param string str This is the CSS property name in valid CSS form.
+   * @return string This is the CSS property name which in valid JS form.
+   */
   function camelCaseCSS(str) {
-    var segments = str.split('-'),
-        segsLen = segments.length,
-        i,
-        newString = '',
-        currSeg;
-    
-    // Camel case all segments except the first.
-    // Probably could do with some optimisation.
-    newString = segments[0];
-    for (i = 1; i < segsLen; i++) {
-      currSeg = segments[i]
-      newString = newString 
-                + currSeg.substring(0, 1).toUpperCase()
-                + currSeg.substring(1);
-    }
-    return newString;
+    return str.replace(/\-([a-z])/g, 
+		       function (whole, section) {
+			 return section.toUpperCase();
+		       });
   }
-  
+   
+  /**
+   * Determine if property given is a valid CSS property.
+   * 
+   * We have to do this so we can determine how to treat it. Right now
+   * Jas is one step ahead of CSS and has 2 valid types of object inside
+   * a rule body: it can have properties and more rules (nested rules)
+   */ 
   function isValidCSSProperty(element, property) {
     if (typeof element.style[property] != "undefined") {
       return true;
@@ -32,7 +36,12 @@
     return false;
   }
   
+   
   function jas(rules, context) {
+    if (typeof rules == "undefined") {
+      return;
+    }
+    
     context = context || document;
     
     var rule,
@@ -47,10 +56,23 @@
       properties  = rules[rule];
       elements    = select(rule);
       elementsLen = elements.length;
+      
+      // Behave like CSS. If no elements match, just move right on.
+      if (elementsLen < 1) {
+	continue;
+      }
+      
       for (i = 0; i < elementsLen; i++) {
         for (propName in properties) {
           element = elements[i];
+	  // We must have the property names in forms JS can accept.
+	  // It may be a good idea to build a table of commonly
+	  // used properties.
           jsPropName = camelCaseCSS(propName);
+	  
+	  // We need to know if this is a valid property to determine
+	  // how to treat it. If it's not valid, we must assume it's a
+	  // nested rule. It's all about traversin' that tree.
           if (isValidCSSProperty(element, jsPropName)) {
             element.style[jsPropName] = properties[propName];            
           }
@@ -68,13 +90,6 @@
   jas.rgb = function (r, g, b) {
     return "rgb(" + r + ", " + g + ", " + b + ")";
   };
-  
-  jas.html = function (element) {
-    console.log(this);
-    return 'one';
-  };
-  
-  jas.ELEMENTS
   
   // Global it!
   window.jas = jas;
